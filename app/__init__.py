85e6935bbde4ca297910edcdca713ed9b9f02387
Itai Sevitt <isevitt@gmail.com>
2021-07-30 12:02:47 +0300

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -2,9 +2,11 @@ from flask import Flask
 from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import create_engine
+from flask_cors import CORS
 
 # Initialization
 application = Flask(__name__, template_folder="../templates", static_folder="../static")
+cors = CORS(application)
 application.config.from_object(Config)
 DB_URI = application.config['SQLALCHEMY_DATABASE_URI']
 engine = create_engine(DB_URI)
